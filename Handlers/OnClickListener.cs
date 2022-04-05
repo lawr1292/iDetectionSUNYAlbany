95229108ca293163daad3a01c6320ac0663ac5f2
Ryan Law <78871247+lawr1292@users.noreply.github.com>
2022-04-05 14:58:22 -0500

    Add files via upload

---
+++ Handlers/OnClickListener.cs
@@ -0,0 +1,27 @@
+﻿using Android.Views;
+using System;
+using static Android.Views.View;
+
+namespace CamApp2
+{
+    class OnClickListener : Java.Lang.Object, IOnClickListener
+    {
+        private readonly Action action;
+
+        public OnClickListener(Action callback)
+        {
+            this.action = callback;
+        }
+        /*
+        public void Onclick(View v)
+        {
+            this.action?.Invoke();
+        }
+        */
+
+        public void OnClick(View v)
+        {
+            this.action?.Invoke();
+        }
+    }
+}
